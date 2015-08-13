f8cdee48acd47cec2b01b812c94cf63be521e064
Rohan Shah <github@bookshah.com>
2015-08-13 15:09:11 +1000

    Shifted conversion function to own file to avoid warning

--- RPackage/R/exactCounts-class.R
+++ RPackage/R/exactCounts-class.R
@@ -16,8 +16,3 @@ setMethod(f = "show", signature = "exactCounts", definition = function(object)
 	result <- capture.output(object@counts)
 	cat(tail(result, -1), sep="\n")
 })
-setAs("exactCounts", "exactSpectra", def = function(from, to)
-{
-	binomialCoefficients <- chooseZ(length(from@counts)-1, 0:(length(from@counts)-1))
-	return(new(to, spectra = as.bigq(from@counts, binomialCoefficients), graph = from@graph, call = from@call, start = from@start, end = from@end))
-})
